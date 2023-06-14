df50ded77a1553169a0087fcd4965efea1ce9b26
Richard Lawrence <rarensu@tamu.edu>
2023-06-14 11:53:37 -0500

    Update attack-all.sh

--- attack-all.sh
+++ attack-all.sh
@@ -1,4 +1,5 @@
-stations=$(awk 'BEGIN { FS = "," } NR>5 { print $1 }' ${1}.csv)
+file=$(ls -1 $(1)*csv | tail -n1)
+stations=$(awk 'BEGIN { FS = "," } NR>5 { if(length($1)>8) print $1 }' $file)
 for station in $stations
 do
 echo attacking $station
