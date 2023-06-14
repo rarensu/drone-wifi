5b116dc1f3a744a6d72e6c4dac764e27190e456a
Richard Lawrence <rarensu@tamu.edu>
2023-06-14 10:29:33 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+ifconfig wlan1 down
+iwconfig wlan1 mode monitor channel 1
+ifconfig wlan1 up
+export GREEN=54:5a:a6:40:36:a7
+export BLUE=54:5a:a6:99:a1:98
+export YELLOW=90:97:d5:21:c3:8A
+export PINK=90:97:d5:21:c6:b5
+export BROWN=90:97:d5:c6:f1:43
