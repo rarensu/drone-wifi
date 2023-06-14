f12c3d0f594c61729f2fd4e424ef133fd166581b
Richard Lawrence <rarensu@tamu.edu>
2023-06-14 11:56:01 -0500

    Update attack-all.sh

--- attack-all.sh
+++ attack-all.sh
@@ -1,9 +1,11 @@
+bash setup.sh
 file=$(ls -1 $(1)*csv | tail -n1)
 stations=$(awk 'BEGIN { FS = "," } NR>5 { if(length($1)>8) print $1 }' $file)
 for station in $stations
 do
-echo attacking $station
+echo attacking phone $station
 bash attack-one.sh $1 $station
+echo 
 done
-echo attacking any
+echo attacking any phone
 bash attack-any.sh $1
