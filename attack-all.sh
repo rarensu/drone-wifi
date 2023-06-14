54e6a362036e0c6f539f7514f9ef5f2818eb80bd
Richard Lawrence <rarensu@tamu.edu>
2023-06-14 11:31:07 -0500

    Update attack-all.sh

--- attack-all.sh
+++ attack-all.sh
@@ -1,8 +1,8 @@
-stations=$(awk 'BEGIN { FS = "," } NR>5 { print $1 }' ${1}.csv
+stations=$(awk 'BEGIN { FS = "," } NR>5 { print $1 }' ${1}.csv)
 for station in $stations
 do
 echo attacking $station
-./attack-one.sh $1 $station
+bash attack-one.sh $1 $station
 done
 echo attacking any
-./attack-any.sh $1
+bash attack-any.sh $1
